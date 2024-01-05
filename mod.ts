31b9673cd9681f14af16a121e7fe7d618c42ac61
zhangfuxing <zhangfuxing@cmict.chinamobile.com>
2024-01-05 11:34:38 +0800

    format code

--- mod.ts
+++ mod.ts
@@ -128,7 +128,7 @@ const ANSI_PATTERN = new RegExp(
     "[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)",
     "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TXZcf-nq-uy=><~]))",
   ].join("|"),
-  "g"
+  "g",
 );
 
 function stripAnsiCode(string: string): string {
